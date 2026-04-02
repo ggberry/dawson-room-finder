85082af581366e35d78a715da4dfa4993fb83412
ggberry <ggsoleil97@gmail.com>
2026-04-01 20:28:10 -0400

    Removed app.run

--- app.py
+++ app.py
@@ -230,5 +230,5 @@ if __name__ == '__main__':
     print("  Dawson Classroom Finder")
     print("=" * 50)
 
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
     # gunicorn app:app --bind 0.0.0.0:$PORT
